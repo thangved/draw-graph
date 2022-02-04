ab275f0c0616fbe51efb3fa32336526d7ea3ad9f
thangved <thangcreation@gmail.com>
2022-02-04 12:08:00 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import core from './core';
 import intro from './intro';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 core()
 if (!JSON.parse(localStorage.getItem('intro_showed') || 'false')) {
     intro()
