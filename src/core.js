0b087d7868356c42211e3998115c4eec699d0f50
thangved <thangcreation@gmail.com>
2022-02-05 09:14:17 +0700

    update search

--- src/core.js
+++ src/core.js
@@ -3,20 +3,34 @@ import Graph from 'graph-board'
 export default function core() {
 
     const g = new Graph({
-        directed: true,
-        showDistance: true,
+        directed: false,
+        showDistance: false,
         showGrid: true,
+        character: true,
     })
+
+    let searchType = 0
+
+    // g.addNode(1)
+    // g.addNode(2)
+    // g.addNode(3)
+    // g.addNode(4)
+    // g.addEdge(1, 4)
+    // g.addEdge(1, 2)
+    // g.addEdge(2, 3)
+    // g.addEdge(2, 4)
 
     g.appendTo('#canvas')
 
     const edgesComponent = document.getElementById('edges')
     const guide = document.getElementById('guide')
     const optionsComponent = document.getElementById('optionsComponent')
+    const firstSearch = document.getElementById('firstSearch')
 
     function updateData() {
         updateEdges()
         updateTable()
+        updateSearchNodes()
     }
 
     function updateEdges() {
@@ -71,18 +85,74 @@ export default function core() {
 
     }
 
-    updateData()
-    setInterval(updateData, 1000)
+    function updateSearchNodes() {
+        const searchNodes = firstSearch.querySelector('#searchNodes')
+        searchNodes.innerHTML = ``
+
+        g.getNodes().forEach((node, index) => {
+            const button = document.createElement('button')
+            button.className = 'btn btn-outline-primary'
+            button.innerText = node.label
+            searchNodes.appendChild(button)
+
+            button.onclick = () => {
+                switch (searchType) {
+                    case 0:
+                        updateSearchResult(g.deepFirstSearch(index + 1))
+                        break
+                    case 1:
+                        updateSearchResult(g.breadthFirstSearch(index + 1))
+                        break
+                }
+            }
+        })
+    }
+
+    function updateSearchResult(data) {
+        const deleteResult = firstSearch.querySelector('#deleteResult')
+        const result = firstSearch.querySelector('#edges')
+        result.innerHTML = ``
+
+        data.forEach(e => {
+            result.innerHTML += `<li class="edge list-group-item">
+            <span>
+                ${g.getNodes()[e.from - 1].label}
+            </span> 
+            <div>
+                <i class="fas fa-long-arrow-alt-right"></i>
+            </div>
+            <span>
+                ${g.getNodes()[e.to - 1].label}
+            </span>
+        </li>`
+        })
+
+        deleteResult.onclick = () => {
+            result.innerHTML = ''
+            g.motionStop()
+        }
+
+    }
+
 
     document.getElementById('removeNodeButton')
-        .addEventListener('click', () => g.removeNode(g.nodes.length))
+        .onclick = () => g.removeNode(g.nodes.length)
 
     const matrixNodeNode = document.getElementById('matrixNodeNode')
     const optionTab = document.getElementById('optionTab')
-    const tabButtons = optionTab.querySelectorAll('li')
+    const tabButtons = optionTab.querySelectorAll('.nav-item')
+    const firstSearchButtons = firstSearch.querySelectorAll('.nav-item')
+
+    firstSearchButtons.forEach((ele, index) => [
+        ele.onclick = () => {
+            firstSearchButtons.forEach(e => e.querySelector('a').classList.remove('active'))
+            firstSearchButtons[index].querySelector('a').classList.add('active')
+            searchType = index
+        }
+    ])
 
     tabButtons.forEach((e, i) => {
-        e.addEventListener('click', event => {
+        e.onclick = () => {
             tabButtons.forEach(e => e.querySelector('a').classList.remove('active'))
             e.querySelector('a').classList.add('active')
 
@@ -90,16 +160,27 @@ export default function core() {
             guide.style.display = 'none'
             optionsComponent.style.display = 'none'
             matrixNodeNode.style.display = 'none'
+            firstSearch.style.display = 'none'
 
-            if (i === 0)
-                optionsComponent.style.display = 'block'
-            if (i === 1)
-                edgesComponent.style.display = 'block'
-            if (i === 2)
-                matrixNodeNode.style.display = 'block'
-            if (i === 3)
-                guide.style.display = 'block'
-        })
+            switch (i) {
+                case 0:
+                    optionsComponent.style.display = 'block'
+                    break
+                case 1:
+                    edgesComponent.style.display = 'block'
+                    break
+                case 2:
+                    matrixNodeNode.style.display = 'block'
+                    break
+                case 3:
+                    firstSearch.style.display = 'block'
+                    break
+
+                default:
+                    guide.style.display = 'block'
+
+            }
+        }
     })
 
     window.addEventListener('mousedown', event => {
@@ -145,35 +226,35 @@ export default function core() {
         optionsMenu.className = 'btn-group-vertical'
         optionsMenu.style = `width: 200px;position: fixed; left: ${event.clientX}px; top: ${event.clientY}px; background: #fff; z-index: 100;border: 1px #ddd solid;border-radius: 5px;box-shadow: 0 0 5px #00000020;`
 
-        const button = document.createElement('button')
-        button.className = 'btn btn-light'
-        button.innerHTML = 'Thêm đỉnh'
-        button.onclick = () => {
+        const button0 = document.createElement('button')
+        const button1 = document.createElement('button')
+        const button2 = document.createElement('button')
+        const button3 = document.createElement('button')
+
+        button0.className = 'btn btn-light'
+        button0.innerHTML = 'Thêm đỉnh'
+        button0.onclick = () => {
             const { x, y } = g.board.clientPosition
             g.addNode(g.nodes.length + 1, x, y)
         }
 
-        const button1 = document.createElement('button')
         button1.className = 'btn btn-light'
         button1.innerHTML = 'Xóa đỉnh'
         button1.onclick = () => g.removeNode(g.nodes.length)
 
-        const button2 = document.createElement('button')
-        button2.disabled = true
         button2.className = 'btn btn-light'
         button2.innerHTML = 'Duyệt theo chiều rộng'
+        button2.onclick = () => tabButtons[3].click()
 
-        const button3 = document.createElement('button')
-        button3.disabled = true
         button3.className = 'btn btn-light'
         button3.innerHTML = 'Duyệt theo chiều sâu'
+        button3.onclick = () => tabButtons[3].click()
 
-        optionsMenu.append(button, button1, button2, button3)
+        optionsMenu.append(button0, button1, button2, button3)
         document.body.append(optionsMenu)
     }
 
     window.addEventListener('contextmenu', addContextMenu)
-
 
     document.getElementById('directedToggle').addEventListener('click', () => {
         g.directed = !g.directed
@@ -191,4 +272,6 @@ export default function core() {
         g.character = !g.character
     })
 
+    updateData()
+    setInterval(updateData, 1000)
 }
